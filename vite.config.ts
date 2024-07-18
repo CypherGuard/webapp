a3a35e9185dc0adcd481267de8e4e258a7890322
lguerra <lylian.gr@protonmail.com>
2024-07-18 09:43:21 +0200

    add https

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import mkcert from 'vite-plugin-mkcert'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [nodePolyfills(), mkcert(), react()],
-  server: {
-    watch: {
-      usePolling: true,
+export default ({ mode }: {mode: string}) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  
+  // import.meta.env.VITE_NAME available here with: process.env.VITE_NAME
+  // import.meta.env.VITE_PORT available here with: process.env.VITE_PORT
+  let plugins = [nodePolyfills(), react()];
+  let https_plugin = [nodePolyfills(), react(), mkcert()];
+  
+  return defineConfig({
+    plugins: process.env.VITE_HTTPS === 'true' ? https_plugin : plugins,
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      port: parseInt(process.env.VITE_PORT || '3000'),
+      host: true,
     },
-    port: 3000,
-    host: true,
-  },
-})
+  })
+}
