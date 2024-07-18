079461bc9c11331a0e6f9c891addfc08ea3b5214
lguerra <lylian.gr@protonmail.com>
2024-07-18 09:35:43 +0200

    https

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), react()],
+  plugins: [nodePolyfills(), mkcert(), react()],
   server: {
     watch: {
       usePolling: true,
     },
     port: 3000,
-    host: true
+    host: true,
   },
 })
